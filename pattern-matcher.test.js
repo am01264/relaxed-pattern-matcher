8afe6612660beeb0e24c07be22648dd497691b8d
Andrew McAuley <git@andrewmcauley.co.uk>
2023-12-20 22:06:39 +0000

    Updated tests to current feature set

--- pattern-matcher.test.js
+++ pattern-matcher.test.js
@@ -1,4 +1,4 @@
-import { $rest, pattern } from './pattern-matcher.js';
+import { $rest, failure, pattern } from './pattern-matcher.js';
 import { assertEquals } from "https://deno.land/std@0.209.0/assert/assert_equals.ts";
 
 Deno.test('should match pattern with object', () => {
@@ -31,57 +31,117 @@ Deno.test('should match pattern with array', () => {
 });
 
 
-
-
-
-
-// describe('Pattern Matcher', () => {
-    
-
-//     test('should match pattern with array', () => {
-//         const d = pattern(({sym, $rest}) =>
-//             [
-//                 sym("first"),
-//                 $rest
-//             ])
-//         const result = d(['one', 'dos', 'drie']);
-//         expect(result.get('first')).toBe('one');
-//         expect(result.get('rest')).toEqual(['dos', 'drie']);
-//     });
-
 Deno.test('should match pattern with rest in object', () => {
-        const j = pattern(({sym, $rest}) => 
-            ({
-                name: "Sarah",
-                [$rest]: sym("deets")
-            })
-        )
-        const result = j({
+    const j = pattern(({sym, $rest}) => 
+        ({
             name: "Sarah",
-            tel: "01234 567 980",
-            address: "123 Keep Reading Me, Nowhere"
-        });
+            [$rest]: sym("deets")
+        })
+    )
+    const result = j({
+        name: "Sarah",
+        tel: "01234 567 980",
+        address: "123 Keep Reading Me, Nowhere"
+    });
     assertEquals(result.get('deets'), {
-            tel: "01234 567 980",
-            address: "123 Keep Reading Me, Nowhere"
-        });
+        tel: "01234 567 980",
+        address: "123 Keep Reading Me, Nowhere"
+    });
+});
+
+Deno.test('should return failure when pattern does not match', () => {
+    const m = pattern(({sym}) => 
+        ({
+            name: "Bob",
+            relatives: {
+                cousin: sym("name")
+            }
+        })
+    )
+    const result = m({
+        name: "WrongName",
+        relatives: {
+            cousin: "James"
+        }
+    });
+    assertEquals(result, failure);
+});
+
+
+Deno.test('should return failure when two identical pattern symbols do not match in a given thing', () => {
+    const m = pattern(({sym}) => 
+        ({
+            x: sym("diff"),
+            y: sym("diff")
+        })
+    )
+    const result = m({
+        x: 1,
+        y: 2
+    });
+    assertEquals(result, failure);
+});
+
+
+Deno.test('should match two identical pattern symbols with identical results in a given thing', () => {
+    const m = pattern(({sym}) => 
+        ({
+            x: sym("same"),
+            y: sym("same")
+        })
+    )
+    const result = m({
+        x: 1,
+        y: 1
+    });
+    assertEquals(result.get('same'), 1);
+});
+
+
+Deno.test('should match pattern with array when first element does not match', () => {
+
+    // Test should only pass if relaxedArrayMatcher is used in place of arrayMatcher.
+
+    const d = pattern(({sym, $rest}) =>
+        [
+            "one",
+            sym("second"),
+            $rest
+        ])
+    const result = d(['badApple', 'one', 'dos', 'drie']);
+    assertEquals(result.get('second'), 'dos');
+    assertEquals(result.get($rest), ['drie']);
+});
+
+
+Deno.test('should match pattern with multiple object children', () => {
+    const mockAst = (type, value) => ({ type, value })
+
+    const p = pattern(({ sym }) => [
+        mockAst("number", sym("first")),
+        mockAst("operator", "x"),
+        mockAst("number", sym("second"))
+    ]);
+    const result = p([
+        mockAst("number", 5),
+        mockAst("operator", "x"),
+        mockAst("number", 2)
+    ])
+
+    assertEquals(result.get("first"), 5)
+    assertEquals(result.get("second"), 2)
+})
+
+
+Deno.test('does not walk prototype chain', () => {
+    class Mockery { }
+
+    const p = pattern(({ sym }) => {
+        constructor: sym("constructor")
     });
 
-//     test('should return failure when pattern does not match', () => {
-//         const m = pattern(({sym}) => 
-//             ({
-//                 name: "Bob",
-//                 relatives: {
-//                     cousin: sym("name")
-//                 }
-//             })
-//         )
-//         const result = m({
-//             name: "Bob",
-//             relatives: {
-//                 cousin: "Not James"
-//             }
-//         });
-//         expect(result).toBe(Symbol('failure'));
-//     });
-// });
+    const result = p(new Mockery)
+
+
+    assertEquals(result, failure)
+})
